1fa845ddac7dfd24a7e6228e0a3205e6cf1ecbb8
Dan Hughes <github@danhughes.dev>
2024-11-09 16:49:07 +0000

    remove comment

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ for service in "${services[@]}"; do
         sudo tee /etc/systemd/system/$service@.service.d/override.conf <<EOF
 [Service]
 Environment="HC_PING_KEY=$HC_PING_KEY"
-Environment="SYNCOID_OFFSITE_IP=100.72.123.112" # Tailscale IP, unused by non-syncoid services
+Environment="SYNCOID_OFFSITE_IP=100.72.123.112"
 EOF
     fi
 
