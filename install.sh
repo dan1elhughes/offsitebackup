184e426826e2737b535e9504ae40264cd7399121
Dan Hughes <github@danhughes.dev>
2024-11-09 00:06:19 +0000

    add photos, loop installer

--- install.sh
+++ install.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
-# Enable the drive@ service
-
-sudo systemctl enable services/drive@.service
-
-# Install the configuration file
 echo "Enter the HC_PING_KEY:"
 read HC_PING_KEY
-sudo mkdir -p /etc/systemd/system/drive@.service.d
-sudo tee /etc/systemd/system/drive@.service.d/override.conf <<EOF
-[Service]
-Environment="HC_PING_KEY=$HC_PING_KEY"
-EOF
 
-# Copy our generic daily timer to the system
-sudo ln -svf $(pwd)/timers/daily@.timer /etc/systemd/system/daily@.timer
+remotes=(drive photos)
+
+for remote in "${remotes[@]}"; do
+    # Install our templated services and timers
+    sudo systemctl enable services/$remote@.service
+    sudo ln -svf $(pwd)/timers/$remote@.timer /etc/systemd/system/$remote@.timer
+
+    # Install the configuration file
+    sudo mkdir -p /etc/systemd/system/$remote@.service.d
+    sudo tee /etc/systemd/system/$remote@.service.d/override.conf <<EOF
+    [Service]
+    Environment="HC_PING_KEY=$HC_PING_KEY"
+    EOF
+done
+
